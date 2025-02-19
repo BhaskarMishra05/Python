9d0bd2f3e7ef30f904c63e048d88093a1c448c7b
BhaskarMishra05 <bhaskarmishra1590@gmail.com>
2025-02-19 18:47:37 +0530

    Inheritance

---
+++ Inheritance/inhertitance1.py
@@ -0,0 +1,12 @@
+class parent_class:
+    def __init__(self,fname,lname):
+        self.fname = fname 
+        self.lname = lname
+    def useless(self):
+        print(self.fname , self.lname)
+class child_class(parent_class):
+    pass
+# obj=parent_class() -> NO NEED TO CREATE AN OBJECT.
+print("Make a variable and store the vlaue of child_class in it.")
+x = child_class("Bhaskar" , "Mishra")
+x.useless()
