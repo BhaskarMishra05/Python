fb1c9b80b02e6c2dbe1e85d4303ca7becec44635
BhaskarMishra05 <bhaskarmishra1590@gmail.com>
2025-04-26 18:29:12 +0530

    m

--- ML1/iris.py
+++ ML1/iris.py
@@ -1,23 +1,22 @@
 from sklearn import datasets
+import numpy as np
 iris=datasets.load_iris()
-print("Keys of iris dataset: ",iris.keys())
-print()
-print(iris.target_names)
-print(iris.feature_names)
-print(iris.data[:5])
-print(iris.target[:5])
-
-print("Test_+train_split")
 from sklearn.model_selection import train_test_split
 X= iris.data 
 y=iris.target 
+num=X.select_dtypes(include=['int64','float64']).columns.tolist()
+cat=X.select_dtypes(include=['object']).columns.tolist()
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
 print("Training x data: ",X_train.shape)
 print("Testing set size: ",X_test.shape) 
 
-print("Import Decision tree classifers: ")
-from sklearn.tree import DecisionTreeClassifier
-model=DecisionTreeClassifier(max_depth=2)
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+pre=ColumnTransformer([('scaler',StandardScaler(),num),
+                       ('encoder',OneHotEncoder(drop='first'),cat)])
+model=Pipeline([('pred',pre),('lin',LogisticRegression())])
 model.fit(X_train,y_train)
 y_pred = model.predict(X_test)
 print("Predict label: ",y_pred)
@@ -30,17 +29,3 @@ from sklearn.metrics import accuracy_score
 accuracy=accuracy_score(y_test,y_pred)
 print(f"Model accuracy: {accuracy* 100:.2f} %")
 print()
-species_mapping={0: "Setosa", 1: "Versicolor", 2: "Virginica"}
-for actual,pred in zip(y_test[:100],y_pred[:100]):
-    actual_label=species_mapping[actual]
-    pred_label=species_mapping[pred]
-    print("Actual: " , actual_label , "Predicted: " ,pred_label)
-    
-print("\nEnter new data for prediction (sepal length, sepal width, petal length, petal width):")
-new_data = [float(input("Sepal Length: ")), float(input("Sepal Width: ")),
-            float(input("Petal Length: ")), float(input("Petal Width: "))]
-new_data = [new_data]  # Wrapping the input data as a list of lists to match the input format
-
-# Make prediction
-new_prediction = model.predict(new_data)
-print(f"Predicted Species: {species_mapping[new_prediction[0]]}")
---
+++ ML1/precision_score.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+np.random.seed(42)
+n=500
+data={'Area_sqft': np.random.uniform(400,4000,n),
+'Bedrooms': np.random.uniform(1,6,n),
+'Bathrooms': np.random.uniform(1,6,n),
+'Floor': np.random.uniform(1,20,n),
+'Total_Floors': np.random.uniform(1,30,n),
+'Location':np.random.choice(['Mumbai', 'Delhi', 'Bangalore', 'Hyderabad', 'Chennai', 'Pune'],n),
+'Furnishing': np.random.choice(['Furnished', 'Semi-Furnished', 'Unfurnished'],n),
+'Age_of_Property': np.random.randint(0,30,n)}
+
+# Assign VAlues: 
+location_map={'Mumbai':400000, 'Delhi':380000, 
+              'Bangalore':450000, 'Hyderabad':350000, 'Chennai':320000, 'Pune':300000}
+furnishing_map={'Furnished':500000, 'Semi-Furnished':30000, 'Unfurnished':50000}
+df=pd.DataFrame(data)
+base_price=400000
+price=(base_price+df['Area_sqft'] * 900+
+       df['Bedrooms'] *50000 + 
+       df['Bathrooms'] * 20000 + df['Floor']*50000+
+       df['Total_Floors']*100000 + 
+       df['Location'].map(location_map)+
+       df['Furnishing'].map(furnishing_map)-
+       df['Age_of_Property'])
+df["Price"]=np.round(price)
+
+# Importing train_test_split
+from sklearn.model_selection import train_test_split,cross_val_score,KFold
+x=df.drop('Price',axis=1)
+y=df['Price']
+x_train,x_test,y_train,y_test=train_test_split(x,y,random_state=42,test_size=0.2)
+
+#Importing other modules
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
+from sklearn.compose import ColumnTransformer
+preprocessing=ColumnTransformer([('scaler',StandardScaler(),['Area_sqft','Bedrooms','Bathrooms',
+                                                             'Total_Floors','Age_of_Property']),
+                                 ('encoder',OneHotEncoder(drop='first'),['Location','Furnishing'])])
+
+#Importing pipeline and model
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+model=Pipeline([('pre',preprocessing),
+                ('linear',LinearRegression())])
+model.fit(x_train,y_train)
+prediction=model.predict(x_test)
+kf=KFold(n_splits=5,shuffle=True,random_state=42)
+cv=cross_val_score(model,x,y,cv=5)
+cvkf=cross_val_score(model,x,y,cv=kf)
+print(f'CV no Kfold {cv}')
+print(f'CV with Kfold: {cvkf}')
+
+#Importing R2 and MSE
+from sklearn.metrics import r2_score,mean_squared_error,make_scorer, precision_score
+precision = make_scorer(precision_score, average='macro')
+cv=cross_val_score(model,x,y,cv=5,scoring=precision)
+cvkf=cross_val_score(model,x,y,cv=kf)
+print(f'CV no Kfold {cv}')
+print(f'CV with Kfold: {cvkf}')
+print(f'R2 score: {r2_score(y_test,prediction) *100 :.2f}')
+print(f'MSE: {mean_squared_error(y_test,prediction) :.2f}')
